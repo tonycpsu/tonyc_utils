7cea7367eaf1eea445df8871d66dcace97f67564
Tony Cebzanov <tonycpsu@gmail.com>
2019-03-28 01:27:46 -0400

    loggging: Allow on-the-fly reconfiguration of logging.

--- tonyc_utils/logging.py
+++ tonyc_utils/logging.py
@@ -14,11 +14,37 @@ LOG_LEVELS = [
 
 DEFAULT_FORMAT="%(asctime)s [%(module)16s:%(lineno)-4d] [%(levelname)8s] %(message)s"
 
+log_level = None
+stdout_handler = None
+logger = None
+formatter = None
+
+def set_stdout_level(level=log_level):
+    global stdout_handler
+    assert(stdout_handler)
+
+    try:
+        handler = next(h for h in logger.handlers if h.stream == sys.stdout)
+    except StopIteration:
+        return
+
+    handler.setLevel(level)
+
+def add_log_handler(handler):
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+
 def setup_logging(level=0, handlers=[], log_format=DEFAULT_FORMAT, quiet_stdout=False):
 
-    level = LOG_LEVEL_DEFAULT + level
-    if level < 0 or level >= len(LOG_LEVELS):
-        raise Exception("bad log level: %d" %(level))
+    global logger
+    global log_level
+    global stdout_handler
+    global formatter
+
+    log_level = LOG_LEVEL_DEFAULT + level
+    if log_level < 0 or log_level >= len(LOG_LEVELS):
+        raise Exception("bad log level: %d" %(log_level))
     # add "trace" log level
     TRACE_LEVEL_NUM = 9
     logging.addLevelName(TRACE_LEVEL_NUM, "TRACE")
@@ -29,9 +55,9 @@ def setup_logging(level=0, handlers=[], log_format=DEFAULT_FORMAT, quiet_stdout=
     logging.Logger.trace = trace
 
     if isinstance(level, str):
-        level = getattr(logging, level.upper())
+        log_level = getattr(logging, log_level.upper())
     else:
-        level = getattr(logging, LOG_LEVELS[level].upper())
+        log_level = getattr(logging, LOG_LEVELS[log_level].upper())
 
     if not isinstance(handlers, list):
         handlers = [handlers]
@@ -41,16 +67,15 @@ def setup_logging(level=0, handlers=[], log_format=DEFAULT_FORMAT, quiet_stdout=
         log_format,
         datefmt="%Y-%m-%d %H:%M:%S"
     )
-    logger.setLevel(level)
-    outh = logging.StreamHandler(sys.stdout)
-    outh.setLevel(logging.CRITICAL if quiet_stdout else level)
+    logger.setLevel(log_level)
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    set_stdout_level(logging.CRITICAL if quiet_stdout else log_level)
 
-    handlers.insert(0, outh)
+    handlers.insert(0, stdout_handler)
     # if not handlers:
     #     handlers = [logging.StreamHandler(sys.stdout)]
     for handler in handlers:
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
+        add_log_handler(handler)
 
     # logger = logging.basicConfig(
     #     level=level,
@@ -58,7 +83,13 @@ def setup_logging(level=0, handlers=[], log_format=DEFAULT_FORMAT, quiet_stdout=
     #     datefmt="%Y-%m-%d %H:%M:%S"
     # )
 
-    logging.getLogger("requests").setLevel(level+1)
-    logging.getLogger("urllib3").setLevel(level+1)
+    # FIXME: move this elsewhere
+    logging.getLogger("requests").setLevel(log_level+1)
+    logging.getLogger("urllib3").setLevel(log_level+1)
+    return logger
 
-__all__ = ["setup_logging"]
+__all__ = [
+    "setup_logging",
+    "set_stdout_level",
+    "add_log_handler"
+]
