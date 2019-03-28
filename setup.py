3fc010320468a32ab95d2a22db912efcdbf491c9
Tony Cebzanov <tonycpsu@gmail.com>
2019-03-28 01:32:31 -0400

    Bump version: 0.0.0 → 0.0.1.dev0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 name = "tonyc_utils"
 setup(name=name,
-      version='0.0.0',
+      version='0.0.1.dev0',
       description='Various utilities',
       author='Tony Cebzanov',
       author_email='tonycpsu@gmail.com',
